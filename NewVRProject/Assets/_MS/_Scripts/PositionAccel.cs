746ccd4d639cfeb993262277f2ea8c7d895c50dd
souta_miura <miura042910@gmail.com>
2020-10-09 22:10:21 +0900

    HMDとコントローラーの回転を取得

---
+++ NewVRProject/Assets/_MS/_Scripts/PositionAccel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionAccel : MonoBehaviour
+{
+    [SerializeField] private Transform headPos;
+    [SerializeField] private TextMesh meshText = null;
+
+    private float accelValue = 0f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        accelValue = transform.position.y - headPos.position.y;
+
+        if (meshText != null)
+            meshText.text = $"{accelValue}";
+
+        var move = headPos.transform.forward * accelValue;
+
+        transform.position += new Vector3(move.x, 0, move.z);
+    }
+}
