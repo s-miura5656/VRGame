9ab1ac8e27e7e40391c9a88cf6ae2053c650fb1f
souta_miura <miura042910@gmail.com>
2020-10-13 15:58:10 +0900

    地面のシェーダー、空中を飛ぶテスト

--- NewVRProject/Assets/_MS/_Scripts/PositionAccel.cs
+++ NewVRProject/Assets/_MS/_Scripts/PositionAccel.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using OVR;
 
 public class PositionAccel : MonoBehaviour
 {
     [SerializeField] private Transform headPos;
+    [SerializeField] private Transform leftHandPos;
+    [SerializeField] private Transform brushPos;
     [SerializeField] private TextMesh meshText = null;
 
     private float accelValue = 0f;
+    private float boost = 0f;
 
     void Start()
     {
@@ -21,7 +25,18 @@ public class PositionAccel : MonoBehaviour
         if (meshText != null)
             meshText.text = $"{accelValue}";
 
-        var move = headPos.transform.forward * accelValue;
+        boost += OVRInput.Get(OVRInput.RawButton.LIndexTrigger) ? 0.1f : -0.1f;
+
+        if (boost > 10f)
+        {
+            boost = 10f;
+        }
+        else if (boost < 0)
+        {
+            boost = 0;
+        }
+
+        var move = headPos.transform.forward * accelValue * boost;
 
         transform.position += new Vector3(move.x, 0, move.z);
     }
